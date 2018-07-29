0845992570222bdbf54dba76f04bb251e48117d2
Bret Comnes <bcomnes@gmail.com>
2018-07-29 12:25:37 -0700

    Checking it all in

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
-module "github.com/ballpit/vgo-test"
+module github.com/ballpit/vgo-test
 
 require (
-	"github.com/ballpit/vgo-dep1" v1.1.4
-	"github.com/ballpit/vgo-dep2" v1.0.0
+	github.com/ballpit/depthree/v2 v2.0.0
+	github.com/ballpit/vgo-dep1 v1.1.4
+	github.com/ballpit/vgo-dep2 v1.0.0
+	github.com/ballpit/vgo-dep2/v3 v3.0.0
 )
--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 
-	dep1 "github.com/ballpit/vgo-dep1"
-	dep2 "github.com/ballpit/vgo-dep2"
+	"github.com/ballpit/depthree/v2"
+	"github.com/ballpit/vgo-dep1"
+	"github.com/ballpit/vgo-dep2"
 )
 
 func main() {
 	fmt.Printf(dep1.TestFunc() + "\n")
+	fmt.Printf(depthree.Dep3func() + "\n")
 	fmt.Printf(dep2.RootFunc() + "\n")
 	fmt.Printf("Hello, world.\n")
 }
